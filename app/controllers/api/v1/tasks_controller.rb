3ee0b6b4567f337c09b270c4a5f92876439dcfd3
kwamebio <kwameagyemang73@gmail.com>
2024-03-19 17:52:05 +0000

    new methods

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::TasksController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UsersController < ApplicationController
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
