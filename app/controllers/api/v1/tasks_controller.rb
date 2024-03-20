7ca0f53e9076a88d4a6dd2919a7de841b253362c
kwamebio <kwameagyemang73@gmail.com>
2024-03-20 21:46:14 +0000

    crud functions

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,23 +1,33 @@
 class Api::V1::TasksController < ApplicationController
 
   def index
-
+    @tasks = Task.all
+    render json: TaskSerializer.new(@tasks).serializable_hash[:data].map { |task| task[:attributes] }
   end
 
   def show
-
+    @task = Task.find(params[:id])
+    if @task
+      render json: TaskSerializer.new(@task).serializable_hash[:data][:attributes], status: :ok
+    end
   end
 
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      render json: TaskSerializer.new(@task).serializable_hash[:data][:attributes], status: :created
+    else
+      render json: {errors: @task.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def update
-
-  end
-
-  def destroy
-
+    @task = Task.find(params[:id])
+    if @task.update(task_oarams)
+      render json: TaskSerializer.new(@task).serializable_hash[:data][:attributes], status: :ok
+    else
+      render json: {errors: @task.errors.full_messages}
+    end
   end
 
 end
